4b1b34e34e998605a2ed8ae2230dff572dd500ab
Douglas Trindade <douglastrindade22@hotmail.com>
2024-04-18 21:53:08 -0300

    wip: adminjs configuration

---
+++ config/sequelizeCli.js
@@ -0,0 +1,10 @@
+module.exports = {
+  development: {
+    dialect: "postgres",
+    host: "localhost",
+    port: "5432",
+    database: "star_api_development",
+    username: "starflicks",
+    password: "123123123",
+  },
+};
---
+++ src/database/index.ts
@@ -0,0 +1,13 @@
+import { Sequelize } from "sequelize";
+
+export const sequelize = new Sequelize({
+  dialect: "postgres",
+  host: "localhost",
+  port: 5432,
+  database: "star_api_development",
+  username: "starflicks",
+  password: "123123123",
+  define: {
+    underscored: true,
+  },
+});
